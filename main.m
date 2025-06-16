d04ba8246f80eaf7ca182198a6796be9652778a6
Macie <Macie@PC-MADOSAEED>
2025-06-16 12:29:39 -0400

    Added comments and brainstormed framework

---
+++ main.m
@@ -0,0 +1,50 @@
+% Macie Orrell
+% Aerostat sizing tool
+% =========================================================================
+
+%% Initialization
+
+close all
+clear all
+clf
+clc
+
+%% Input desired parameters
+
+% inputs.LiftingGas = 1; % select H or He
+inputs.PayloadWeight = 5; % in kgs
+
+%% Create and plot different balloon shapes for desired size
+
+% Slenderness ratio refers to the length/max diameter of the sheroid shape.
+% Slenderness ratio is represented by lambda and determines aerodynamic 
+% performance of the aerostat. A slenderness ratio of 1 refers to a perfect
+% sphere.
+
+% Axis def 
+% a = length along z
+% b = length along x
+% c = length along y 
+
+% Guess length and Dmax
+length = ;
+b = length /2;
+a = 
+c = D_max;
+%d_factor = linspace(); % se as a bunch of fracs
+d_factor = 1;
+D_max = 1ength * d_factor % set D_max initial to length (sphere case first)
+
+
+
+
+
+% Elliptic profile
+
+% Circular profile
+
+% Parabolic profile
+
+%% Calculate Lifting Gas required in liquid and gas phases
+
+
