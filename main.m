3a90a2f44e47f31b1e09a48d13163b7906e2a357
Macie <Macie@MSI>
2026-02-23 23:01:35 -0500

    cleaned comments

--- main.m
+++ main.m
@@ -55,10 +55,8 @@ for r = 1:1:length(spheroidInputs.slenderness_ratio)
                 W_env = library.Envelope(e).WeightperMeter * surface_area(r);
                 W_tether = library.Tether(t).WeightperMeter * surface_area(r); 
               
-             
                 % Total weight
                 total_weight(r,g,e,t) = W_gas + W_env + W_tether;
-
 
                 % SA calculation
                 total_SA(r,g,e,t) = surface_area(r);
@@ -94,67 +92,3 @@ hold off
 
 
 
-
-% minWeight = spheroids(1).lowest_total_weight;
-% 
-% for i = 2:1:length(spheroids)
-%    if spheroids(i).lowest_total_weight < minWeight
-%        minWeight = spheroids(i).lowest_total_weight;
-%    end
-% end
-
-%% Filter in order of priority criteria
-
-% Filter to only spheroids with the smallest surface area
-% candidates = filter_smallestSA(spheroids);
-% candidates = filter_lowestTotalWeight
-
-
-
-
-
-% % Find spheroid with smallest surface area
-% 
-% minSA = [spheroids(1).surfaceArea, 0]; % initialize with first value
-% 
-% i = 2;
-% while i <= length(spheroids)
-% 
-%     if spheroids(i).surfaceArea < minSA
-%         minSA = [spheroids(i).surfaceArea, i];
-%     end
-% 
-%     i = i + 1;
-% 
-% end
-
-
-        % % Find smallest SA
-        % function obj = find_smallestSA(obj)
-        % 
-        %     obj.smallest_SA = [obj.surfaceArea(1,1,1)];
-        % 
-        %     e=1;
-        %     while e <= length(obj.surfaceArea(:,1,1))
-        %         t=1;
-        %         while t <= length(obj.surfaceArea(1,:,1))
-        %              g=1;
-        %             while g <= length(obj.surfaceArea(1,1,:))
-        %                 if obj.surfaceArea(e,t,g) < obj.smallest_SA
-        %                     obj.smallest_SA = [obj.surfaceArea(e,t,g), e, t, g];
-        %                 end
-        %                 g = g+1;
-        %             end
-        %             t = t+1;
-        %         end
-        %         e = e+1;
-        %     end
-
-
-
-
-
-
-
-
-%% Input desired parameters
